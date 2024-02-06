4e2de5a5f2025dbe0988d4d6fa15c7c8f59e67e3
Neel <pneel294@gmail.com>
2024-02-06 14:10:36 -0500

    added routes for doctor and patient dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
+import PatientDashboard from './components/PatientDashboard/PatientDashboard';
+import DoctorDashboard from './components/DoctorDashboard/DoctorDashboard';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
 
 function App() {
   
@@ -11,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
-         
+          <Route path="/patientDashboard" element={<PatientDashboard/>}/>
+          <Route path="/doctorDashboard" element={<DoctorDashboard/>}/>
         </Routes>
       </Router>
    
