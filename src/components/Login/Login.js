d6053b7fc75ec3291fdcfbf31f3c61e2a861817b
Neel <pneel294@gmail.com>
2024-04-09 09:39:19 -0400

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,22 +36,22 @@ const Login = () =>{
                 };
     
                 try{
-                    const response  = await axios.post("/login", userinfo);
+                    const response  = await axios.post("https://wellnessforever.onrender.com/login", userinfo);
                     if(response.data[0].userType === 'doctor'){
-                        path("/doctorDashboard",{state : response.data[0].userType})
+                        path("https://wellnessforever.onrender.com/doctorDashboard",{state : response.data[0].userType})
 
                     }else if(response.data[0].userType === 'patient'){
                         console.log(response.data[0]);
-                        path("/patientDashboard",{state : response.data[0]})
+                        path("https://wellnessforever.onrender.com/patientDashboard",{state : response.data[0]})
 
                     }else if(response.data[0].userType === 'admin'){
-                        path("/adminDashboard",{state : response.data[0].userType})
+                        path("https://wellnessforever.onrender.com/adminDashboard",{state : response.data[0].userType})
 
                     }else if (response.data === "incorrectPassword"){
                         console.log("Incorrect Password");
     
                     }else if (response.data === "notExists") {
-                        path("/login")
+                        path("https://wellnessforever.onrender.com/login")
                         console.log("User exists");
                     }
     
