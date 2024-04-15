7db2050dd52e027f75fe2b4b2be7418dee2eeea1
Neel <pneel294@gmail.com>
2024-04-14 20:33:36 -0400

    updates

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,6 +9,7 @@ const Login = () =>{
     const[password,setPassword] = useState('');
     const [formErrors,setFormErrors] = useState({});
     const backendUrl = process.env.API;
+    console.log(backendUrl);
     const validateForm = ()=> {
         const errors={};
        
@@ -35,7 +36,7 @@ const Login = () =>{
                 };
     
                 try{
-                    const response  = await axios.post(`/login`, userinfo);
+                    const response  = await axios.post(`${backendUrl}/login`, userinfo);
                     
                     if(response.data[0].userType === 'doctor'){
                         path("/doctorDashboard",{state : response.data[0]})
