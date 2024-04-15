94eea0ca93e90d4044a6f0ea5ba4e3814da4621c
Neel Patel <104414845+Neel2942@users.noreply.github.com>
2024-04-15 17:18:35 -0400

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,7 +10,7 @@ const Login = () =>{
     const[password,setPassword] = useState('');
     const [formErrors,setFormErrors] = useState({});
     const backendUrl = process.env.BACKEND_API;
-    console.log(process.env);
+    console.log(process.env.BACKEND_API);
     const validateForm = ()=> {
         const errors={};
        
